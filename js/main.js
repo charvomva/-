64582d8be69659540968d4eae361b44e755807bf
charvomva <mk28ar@gmail.com>
2022-07-26 16:56:56 +0900

    으... 이제 시작인가...

--- js/main.js
+++ js/main.js
@@ -1,22 +1,12 @@
-const tabBtn = document.querySelectorAll('.tabsMenu li');
-const tabBd = document.querySelectorAll('.tabsBody>div');
-console.log(tabBtn.length); 
-
-
-tabBtn.forEach(function(item,index){
-    item.onclick = function(){
-
-        for( let i = 0; i<tabBtn.length; i++){
-            tabBtn[i].classList.remove('on');
-            tabBd[i].style.display='none';
-        }
-        item.classList.add('on');
-        tabBd[index].style.display='block';
-
-     /*   tabBtn[num].classList.remove('on');
-        this.classList.add('on');
-        num = bb;
-        */
-    };
-
-})
+var swiper = new Swiper(".mySwiper", {
+    speed: 600,
+    parallax: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
